f08e667ca1fd3d8aace89974eb98909eedd3f18e
ILobogamer19 <manuelgornelasn@gmail.com>
2024-03-20 18:30:28 -0300

    Correção

--- client/src/components/FormularioCadastro.js
+++ client/src/components/FormularioCadastro.js
@@ -64,7 +64,7 @@ export default function FormularioMercadoPago() {
     console.log(Valores.Nome);
 
     Axios.post(
-      "https://ornelassignature-server.vercel.app/registro",
+      "https://ornelassignature-server.vercel.app:5000/registro",
       {
         Nome: Valores.Nome,
         Numero: Valores.Numero,
