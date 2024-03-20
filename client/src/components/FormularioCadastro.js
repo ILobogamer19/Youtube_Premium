732e6516b60d9368864981c8e76b73ef239d1fa1
ILobogamer19 <manuelgornelasn@gmail.com>
2024-03-20 18:42:18 -0300

    teste

--- client/src/components/FormularioCadastro.js
+++ client/src/components/FormularioCadastro.js
@@ -64,7 +64,7 @@ export default function FormularioMercadoPago() {
     console.log(Valores.Nome);
 
     Axios.post(
-      "https://ornelassignature-server.vercel.app:5000/registro",
+      "https://ornelassignature-server.vercel.app/registro",
       {
         Nome: Valores.Nome,
         Numero: Valores.Numero,
