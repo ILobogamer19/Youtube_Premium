014a91652a56cec9abd3b2296964f30adf701b39
ILobogamer19 <manuelgornelasn@gmail.com>
2024-03-20 17:48:02 -0300

    Correção de conexão entre cliente e servidor 3.0

--- server/server.js
+++ server/server.js
@@ -17,60 +17,54 @@ app.get("/", (Requisicao, Resposta) => {
   Resposta.send("Servidor rodando");
 });
 
-app.post(
-  "https://ornelassignature-server.vercel.app/registro",
-  (Requisicao, Resposta) => {
-    const { Nome } = Requisicao.body;
-    const { Numero } = Requisicao.body;
-    const { Email } = Requisicao.body;
-    const { Indicacao } = Requisicao.body;
+app.post("/registro", (Requisicao, Resposta) => {
+  const { Nome } = Requisicao.body;
+  const { Numero } = Requisicao.body;
+  const { Email } = Requisicao.body;
+  const { Indicacao } = Requisicao.body;
 
-    console.log("Nome: " + Nome);
+  console.log("Nome: " + Nome);
 
-    let Sintaxe_SQL_Pesquisa_Usuarios = "SELECT email FROM assinantes";
-    let Emails_Registrados;
-    let Teste_Duplicidade = true;
+  let Sintaxe_SQL_Pesquisa_Usuarios = "SELECT email FROM assinantes";
+  let Emails_Registrados;
+  let Teste_Duplicidade = true;
 
-    Banco_De_Dados.query(Sintaxe_SQL_Pesquisa_Usuarios, (erro, Resultado) => {
-      console.log("Executado");
-      return (Emails_Registrados = Resultado);
+  Banco_De_Dados.query(Sintaxe_SQL_Pesquisa_Usuarios, (erro, Resultado) => {
+    console.log("Executado");
+    return (Emails_Registrados = Resultado);
+  });
+
+  setTimeout(() => {
+    console.log("Inicio de repetição de objeto");
+    Emails_Registrados.forEach((Objeto) => {
+      console.log("Verificação de email repetido");
+      if (Email == Objeto.email) {
+        Teste_Duplicidade = false;
+        console.log("Email repetido encontrado");
+      }
     });
 
-    setTimeout(() => {
-      console.log("Inicio de repetição de objeto");
-      Emails_Registrados.forEach((Objeto) => {
-        console.log("Verificação de email repetido");
-        if (Email == Objeto.email) {
-          Teste_Duplicidade = false;
-          console.log("Email repetido encontrado");
+    let Sintaxe_SQL_Insercao =
+      "INSERT INTO assinantes ( nome, numero, email, indicacao) VALUES (?, ?, ?, ?)";
+
+    let Valores_Para_Insercao = [Nome, Numero, Email, Indicacao];
+
+    if (Teste_Duplicidade === true) {
+      console.log("Inserção de assinante");
+      Banco_De_Dados.query(
+        Sintaxe_SQL_Insercao,
+        Valores_Para_Insercao,
+        (erro, resultado) => {
+          console.log(erro);
+          console.log(resultado);
         }
-      });
+      );
+    }
+  }, 500);
+});
 
-      let Sintaxe_SQL_Insercao =
-        "INSERT INTO assinantes ( nome, numero, email, indicacao) VALUES (?, ?, ?, ?)";
-
-      let Valores_Para_Insercao = [Nome, Numero, Email, Indicacao];
-
-      if (Teste_Duplicidade === true) {
-        console.log("Inserção de assinante");
-        Banco_De_Dados.query(
-          Sintaxe_SQL_Insercao,
-          Valores_Para_Insercao,
-          (erro, resultado) => {
-            console.log(erro);
-            console.log(resultado);
-          }
-        );
-      }
-    }, 500);
-  }
-);
-
-app.get(
-  "https://ornelassignature-server.vercel.app/registro",
-  (Requisicao, Resposta) => {
-    Resposta.send("Registro Servidor");
-  }
-);
+app.get("/registro", (Requisicao, Resposta) => {
+  Resposta.send("Registro Servidor");
+});
 
 app.listen(5000, () => console.log("Servidor na porta 5000"));
