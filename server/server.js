ea81e621502fc187302e0f537aafdbb3cc1e31c2
ILobogamer19 <manuelgornelasn@gmail.com>
2024-03-20 22:42:23 -0300

    Teste de dados para banco de dados novo

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ const mysql = require("mysql");
 const cors = require("cors");
 
 const Banco_De_Dados = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  password: "devmanuel2006",
+  host: "youtube.c7cca24a6tib.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "adiminadmin",
   database: "youtube_premium",
   insecureAuth: true,
 });
@@ -25,12 +25,24 @@ app.get("/", (Requisicao, Resposta) => {
 });
 
 app.post("/registro", (Requisicao, Resposta) => {
-  const { Nome } = Requisicao.body;
-  const { Numero } = Requisicao.body;
-  const { Email } = Requisicao.body;
-  const { Indicacao } = Requisicao.body;
+  const { Nome, Numero, Email, Indicacao } = Requisicao.body;
+  const Status_Assinatura = true;
+  const Status_Pagamento = true;
+  const Data_Assinatura = new Date();
+  const Data_Vencimento = Data_Assinatura.setMonth(
+    Data_Assinatura.getMonth() + 1
+  );
+  const Grupo_Assinantes = 1;
 
   console.log("Nome: " + Nome);
+  console.log("Numero: " + Numero);
+  console.log("Email: " + Email);
+  console.log("Indicacao: " + Indicacao);
+  console.log("Status_Assinatura: " + Status_Assinatura);
+  console.log("Status_Pagamento: " + Status_Pagamento);
+  console.log("Data_Assinatura: " + Data_Assinatura);
+  console.log("Data_Vencimento: " + Data_Vencimento);
+  console.log("Grupo_Assinantes: " + Grupo_Assinantes);
 
   let Sintaxe_SQL_Pesquisa_Usuarios = "SELECT email FROM assinantes";
   let Emails_Registrados;
@@ -52,21 +64,35 @@ app.post("/registro", (Requisicao, Resposta) => {
     });
 
     let Sintaxe_SQL_Insercao =
-      "INSERT INTO assinantes ( nome, numero, email, indicacao) VALUES (?, ?, ?, ?)";
+      "INSERT INTO assinantes ( nome, telefone, email, indicacao, status_assinatura, status_pagamento, data_assinatura, data_vencimento, grupo_assinantes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    let Valores_Para_Insercao = [Nome, Numero, Email, Indicacao];
+    let Valores_Para_Insercao = [
+      Nome,
+      Numero,
+      Email,
+      Indicacao,
+      Status_Assinatura,
+      Status_Pagamento,
+      Data_Assinatura,
+      Data_Assinatura,
+      Grupo_Assinantes,
+    ];
 
-    if (Teste_Duplicidade === true) {
-      console.log("Inserção de assinante");
-      Banco_De_Dados.query(
-        Sintaxe_SQL_Insercao,
-        Valores_Para_Insercao,
-        (erro, resultado) => {
-          console.log(erro);
-          console.log(resultado);
-        }
-      );
-    }
+    console.log(
+      "Valores que vão para o banco de dados: " + Valores_Para_Insercao
+    );
+
+    // if (Teste_Duplicidade === true) {
+    //   console.log("Inserção de assinante");
+    //   Banco_De_Dados.query(
+    //     Sintaxe_SQL_Insercao,
+    //     Valores_Para_Insercao,
+    //     (erro, resultado) => {
+    //       console.log(erro);
+    //       console.log(resultado);
+    //     }
+    //   );
+    // }
   }, 500);
 
   Resposta.send(console.log("Executado o servidor"));
